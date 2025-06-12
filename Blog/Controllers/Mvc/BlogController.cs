59f3dc6240ad7d263b63d2ae6c5eb071be7f2de5
TehAutumnCore <naruto13581@gmail.com>
2025-06-12 11:22:03 -0400

    completed rebuilding mvc ui with some bootstrap 5

--- Blog/Controllers/Mvc/BlogController.cs
+++ Blog/Controllers/Mvc/BlogController.cs
@@ -1,1 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Blog.Models;
+using Blog.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace Blog.Controllers.Mvc;
+
+public class BlogViewController : Controller
+{
+    private readonly BlogDbContext _context;
+
+    public BlogViewController(BlogDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var posts = _context.Blogs.OrderByDescending(p => p.Date).ToList();
+        return View(posts);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View(); //Views/Blog/Create.cshtml
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(BlogPost post)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Blogs.Add(post);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(post);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var post = _context.Blogs.Find(id);
+        if (post == null) return NotFound();
+        return View(post); //Views/Blog/Edit.cshtml
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, BlogPost post)
+    {
+        if (id != post.Id) return BadRequest(); //400 Bad request
+        if (!ModelState.IsValid) return View(post);
+
+        _context.Blogs.Update(post);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var post = _context.Blogs.Find(id);
+        if (post == null) return NotFound();
+        return View(post);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var post = _context.Blogs.Find(id);
+        if (post == null) return NotFound();
+
+        _context.Blogs.Remove(post);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+}
--- Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3988e8ffd0ef94c42f95717fede401dc4b5e23d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e7e3bd621e89b34c9512e913a4b1adfd29ebae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
